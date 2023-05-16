9f049ce56fecdcde4d804aa5ea21b4dd4659cb53
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2023-05-16 15:30:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const dropDownArrows = Array.from(document.querySelectorAll(".arrow"));
 const dropDownTxts = Array.from(document.querySelectorAll(".arrow-text"));
+const dropDownContent1 = document.querySelector(".dropdown-content-1");
+const dropDownContent2 = document.querySelector(".dropdown-content-2");
 
 dropDownArrows.forEach((arrow, index) => {
   arrow.addEventListener("click", () => {
@@ -17,4 +19,12 @@ dropDownArrows.forEach((arrow, index) => {
       arrow.classList.add("active");
     }
   });
+});
+
+dropDownTxts[0].addEventListener("click", ()=>{
+  dropDownContent1.classList.toggle("active-content");
+});
+
+dropDownTxts[1].addEventListener("click", ()=>{
+  dropDownContent2.classList.toggle("active-content");
 });
